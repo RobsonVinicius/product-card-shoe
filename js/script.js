a7758275511c7bf59fd82a14f38e6478ce99b0c0
Robson Vinicius <robsondgn@outlook.com>
2020-07-31 14:06:52 -0300

    Criando animação da mudança de cor do fundo gradiente conforme a seleção da cor

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const colors = document.querySelectorAll('.color');
 const shoes = document.querySelectorAll('.shoe');
 const gradients = document.querySelectorAll('.gradient');
 
+let prevColor = "blue";
+
 function changeSize() {
   sizes.forEach(size => size.classList.remove('active'));
   this.classList.add('active');
@@ -12,6 +14,8 @@ function changeColor() {
   let primary = this.getAttribute('primary');  
   let color = this.getAttribute('color');
   let shoe = document.querySelector(`.shoe[color="${color}"]`)
+  let gradient = document.querySelector(`.gradient[color="${color}"]`);
+  let preGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
 
   colors.forEach(c => c.classList.remove('active'));
   this.classList.add('active');
@@ -21,6 +25,11 @@ function changeColor() {
   shoes.forEach(s => s.classList.remove('show'));
   shoe.classList.add('show');
 
+  gradients.forEach(g => g.classList.remove('first', 'second'));
+  gradient.classList.add('first')
+  preGradient.classList.add('second');
+
+  prevColor = color;
 }
 
 sizes.forEach(size => size.addEventListener('click', changeSize));

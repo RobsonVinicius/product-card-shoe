07e97e176754c602542696fc140daca27d770701
Robson Vinicius <robsondgn@outlook.com>
2020-07-31 00:52:58 -0300

    Criação da function changeColor para os botões de mudança de cor

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const sizes = document.querySelectorAll('.size');
+const colors = document.querySelectorAll('.color');
 
 function changeSize() {
   sizes.forEach(size => size.classList.remove('active'));
   this.classList.add('active');
 }
 
+function changeColor() {
+  colors.forEach(c => c.classList.remove('active'));
+  this.classList.add('active');
+}
 
 sizes.forEach(size => size.addEventListener('click', changeSize));
+colors.forEach(c => c.addEventListener('click', changeColor));
